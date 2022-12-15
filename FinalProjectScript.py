582ff44114aa3687e8cb6bba292c60fcc6f2ff7e
AnnaSLRomero <asromero@ualberta.ca>
2022-12-14 17:19:24 -0700

    edits

--- FinalProjectScript.py
+++ FinalProjectScript.py
@@ -1,15 +1,13 @@
 # 505_FinalProject
 ## By: Anna Romero
 
-#This is testing item recognition memory.
-#Participants are shown 10 images in the beginning of the block.
-#Afterwards, they are tested on all the images plus 5 new images. They are to press 1 if they saw the image before ("old" condition) and they have to press 2 if was not presented earlier ("new" condition). Therefore, overall there are 15 images during testing.
+#This is testing item recognition memory. Participants are shown 10 images in the beginning of the #block. Afterwards, they are tested on all the images plus 5 new images. They are to press 1 if they #saw the image before ("old" condition) and they have to press 2 if was not presented earlier ("new" #condition). Therefore, overall there are 15 images during testing.
 
-#The stimuli I am using are screenshots of the unity assets we are currently using in our VR videos. We want to test item recognition for objects we have created in our scenes and I wanted to see if we could do this through psychopy.
+#The stimuli I am using are screenshots of the unity assets we are currently using in our VR videos. #We want to test item recognition for objects we have created in our scenes and I wanted to see if we #could do this through psychopy. I have 2 blocks here, just for the class, but I think for us we will #just have 1 block.
 
-#The images are to be presented for 1500 msecs or 1.5 seconds during the presentation stage. During testing, the image will stay up until "1" or "2" are pressed. 
+#The images are to be presented for 1500 msecs or 1.5 seconds during the presentation stage. During #testing, the image will stay up until "1" or "2" are pressed.
 
-#In the csv file that is created at the end records the Trial (no block, because there is only 1 block), the accuracy, and response time of the participant.
+#In the csv file that is created at the end records the SubjectID, experimenter initials, Block #Number, Trial number, acuuracy, and response times.
 
 
 
@@ -65,7 +63,7 @@ sub_dir = os.path.join(main_dir,'sub_info',filename)
 #-number of trials and blocks *
 nTrials = 15
 nPresent = 10
-nBlocks = 1
+nBlocks = 2
 totalTrials = nTrials*nBlocks
 nEach = int(totalTrials/2)
 
@@ -182,7 +180,7 @@ for iblock in range(nBlocks):
                 
         if keys:
             responseTimes[overallTrial] = trial_timer.getTime() 
-            if test[overallTrial][0] == 'o':
+            if test[itrial][0] == 'o':
                 if keys[0] == '1':
                     accuracies[overallTrial] = 'Correct'
                 else:
@@ -198,7 +196,7 @@ for iblock in range(nBlocks):
          ', Trial:', 
          itrial+1, 
          ',', 
-         test[overallTrial][0], 
+         test[itrial][0], 
          ':', 
          accuracies[overallTrial], 
          ', RT:', 
@@ -209,6 +207,9 @@ for iblock in range(nBlocks):
 
 #create a data frame from a dictionary of collected data then make it to csv file)
 df = pd.DataFrame(data={
+ "SubjectID" : exp_info["SubjectID"],
+ "Experimenter" : exp_info["Experimenter"],
+ "Block Number": blockNumbers,
  "Trial Number": trialNumbers, 
  "Accuracy": accuracies, 
  "Response Time": responseTimes,
